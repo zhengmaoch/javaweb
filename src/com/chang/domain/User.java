4db67b67070c71e5919824aee5ad32873ac16890
zhengmaoch <zhengmaoch@qq.com>
2019-01-15 08:53:51 +0800

    添加lombok注解标签

--- src/com/chang/domain/User.java
+++ src/com/chang/domain/User.java
@@ -1,8 +1,10 @@
 package com.chang.domain;
 
+import lombok.Data;
+
 import java.util.Date;
 
-
+@Data
 public class User {
     private String id;
     private String username;
@@ -10,52 +12,4 @@ public class User {
     private String email;
     private Date birthday;
     private String nikename;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Date getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
-
-    public String getNikename() {
-        return nikename;
-    }
-
-    public void setNikename(String nikename) {
-        this.nikename = nikename;
-    }
 }
